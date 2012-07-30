7e9cb1e4fe22506644fc7891c26b807108b3e05a
AmmoniteNetworks <chris@ammonitenetworks.com>
2012-07-29 19:48:18 -0700

    Clean up

--- plg_content_access/tmpl/default.php
+++ plg_content_access/tmpl/default.php
@@ -1,6 +1,6 @@
 <form method="post" action="">
 	
-	<input type="text" name="access_username">
-	<input type="text" name="access_password">
-	<input type="submit" value="submit" name="submit">
+	<input type="text" name="access_username"><br>
+	<input type="text" name="access_password"><br>
+	<input type="submit" value="submit" class="btn button" name="submit">
 </form>
